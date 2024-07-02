db7725d0623b3a529207bafd11a0d44d6b961e31
ImAidanJ <160815252+ImAidanJ@users.noreply.github.com>
2024-07-02 13:24:34 -0400

    Fixed a minor issue.
    I forgot to add the server side stuff.

--- server/server.lua
+++ server/server.lua
@@ -20,7 +20,40 @@ end)
 
 
 -- Script Server Code
+RegisterCommand(Config.CommandName, function(source, args, rawCommand)
+    if not Config.UseAcePermissions or IsPlayerAceAllowed(source, Config.AcePermissionString) then
 
+        if Config.CountdownEnabled then
+            TriggerClientEvent('chat:addMessage', -1, {
+                color = {255, 255, 255},
+                multiline = true,
+                args = {"[^3CSRP^0] Starting countdown..."}
+            })
+
+            for i = Config.CountdownDuration, 1, -1 do
+                Citizen.Wait(1000) -- Wait for 1 second
+                TriggerClientEvent('chat:addMessage', -1, {
+                    color = {255, 255, 255},
+                    multiline = true,
+                    args = {"[^3CSRP^0]", tostring(i)}
+                })
+            end
+        end
+
+        TriggerClientEvent("AJ:clearAllVehicles", -1)
+
+        Citizen.Wait(1000) -- 1 Seconds
+
+        if Config.ClearChatAfterCleanup then
+            TriggerClientEvent('chat:clear', -1)
+        end 
+
+        TriggerClientEvent("chat:addMessage", -1, { template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(46, 235, 94, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', args = {"MassDV has been Completed."}})
+        
+    else
+        TriggerClientEvent("chat:addMessage", source, {template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(255, 0, 0, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', args = {"You do not have permission to use this command."}})    
+    end
+end, false)
 
 -- Credits --
 -- Don't be weird.. Leave my Credits!!
